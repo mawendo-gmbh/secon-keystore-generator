f70652aed669cabb3d619867f0010135a3a90534
fongie <mkorlinge@hotmail.com>
2021-04-22 16:33:55 +0200

    checkstyle formatting

--- src/main/java/com/mawendo/secon/SeconKeyStoreGenerator.java
+++ src/main/java/com/mawendo/secon/SeconKeyStoreGenerator.java
@@ -1,9 +1,5 @@
 package com.mawendo.secon;
 
-import org.apache.commons.cli.*;
-import org.tinylog.Logger;
-import org.tinylog.configuration.Configuration;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -14,6 +10,15 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.tinylog.Logger;
+import org.tinylog.configuration.Configuration;
 
 public class SeconKeyStoreGenerator {
   private static final CertificateFactory CERTIFICATE_FACTORY;
@@ -30,12 +35,15 @@ public class SeconKeyStoreGenerator {
     }
   }
 
-  /** Run generator CLI. */
+  /**
+   * Run generator CLI.
+   */
   public static void main(String[] args) {
     Options options = new Options();
     options.addOption(
         new Option(
-            "k", "insurance-keys", true, "path of the insurance keys file (default: " + DEFAULT_KEY_PATH + ")"));
+            "k", "insurance-keys", true,
+            "path of the insurance keys file (default: " + DEFAULT_KEY_PATH + ")"));
 
     options.addOption(
         new Option(
@@ -44,21 +52,25 @@ public class SeconKeyStoreGenerator {
             true,
             "path of output .p12 key store file (default: " + DEFAULT_KEY_STORE_PATH + ")"));
 
-      options.addOption(
-              new Option(
-                      "p",
-                      "private-key",
-                      true,
-                      "(optional) path of your private key (PKCS1 .pem file beginning with '--BEGIN RSA PRIVATE KEY--')"));
+    options.addOption(
+        new Option(
+            "p",
+            "private-key",
+            true,
+            "(optional) path of your private key (PKCS1 .pem file beginning with"
+                + " '--BEGIN RSA PRIVATE KEY--')"));
 
-      options.addOption(
-              new Option(
-                      "c",
-                      "chain",
-                      true,
-                      "(optional) path of your certificate chain (.p7c file received from ITSG)"));
+    options.addOption(
+        new Option(
+            "c",
+            "chain",
+            true,
+            "(optional) path of your certificate chain (.p7c file received from ITSG)"));
 
-      options.addOption(new Option("a", "alias", true, "the alias of your private key in the keystore (default: " + DEFAULT_PRIVATE_KEY_ALIAS + ")"));
+    options.addOption(new Option("a", "alias", true,
+        "the alias of your private key in the keystore (default: "
+            + DEFAULT_PRIVATE_KEY_ALIAS
+            + ")"));
     options.addOption(new Option(null, "key-store-password", true, "password of key store file"));
 
     options.addOption(new Option(null, "debug", false, "enable debug logging"));
@@ -84,14 +96,15 @@ public class SeconKeyStoreGenerator {
 
     boolean shouldEmbedPrivateKey = false;
     if (cmd.hasOption("private-key") || cmd.hasOption("chain")) {
-        if (!cmd.hasOption("chain") || !cmd.hasOption("private-key")) {
-            Logger.error(
-                    "To embed your private key in the keystore you must provide both a PEM private key (--private-key) and a P7C certificate chain (--chain)"
-            );
-            formatter.printHelp(SeconKeyStoreGenerator.class.getSimpleName(), options);
-            System.exit(1);
-        }
-        shouldEmbedPrivateKey = true;
+      if (!cmd.hasOption("chain") || !cmd.hasOption("private-key")) {
+        Logger.error(
+            "To embed your private key in the keystore you must provide both a PEM private key"
+                + " (--private-key) and a P7C certificate chain (--chain)"
+        );
+        formatter.printHelp(SeconKeyStoreGenerator.class.getSimpleName(), options);
+        System.exit(1);
+      }
+      shouldEmbedPrivateKey = true;
     }
 
     if (cmd.hasOption("debug")) {
@@ -99,7 +112,8 @@ public class SeconKeyStoreGenerator {
     }
 
     Path keyFilePath =
-        Paths.get(cmd.hasOption("insurance-keys") ? cmd.getOptionValue("insurance-keys") : DEFAULT_KEY_PATH);
+        Paths.get(cmd.hasOption("insurance-keys") ? cmd.getOptionValue("insurance-keys") :
+            DEFAULT_KEY_PATH);
 
     Path keyStoreFilePath =
         Paths.get(
@@ -113,46 +127,50 @@ public class SeconKeyStoreGenerator {
             : new String(System.console().readPassword("Enter password for key store: "));
 
     String privateKeyAlias =
-            cmd.hasOption("alias")
+        cmd.hasOption("alias")
             ? cmd.getOptionValue("alias")
-                    : DEFAULT_PRIVATE_KEY_ALIAS;
+            : DEFAULT_PRIVATE_KEY_ALIAS;
 
-      try {
-          HealthInsuranceKeyStoreGenerator healthInsuranceKeyStoreGenerator = new HealthInsuranceKeyStoreGenerator(CERTIFICATE_FACTORY);
-          healthInsuranceKeyStoreGenerator.loadHealthInsuranceKeys(keyFilePath);
-          KeyStore keystore = healthInsuranceKeyStoreGenerator.generateKeyStore(keyStorePassword);
+    try {
+      HealthInsuranceKeyStoreGenerator healthInsuranceKeyStoreGenerator =
+          new HealthInsuranceKeyStoreGenerator(CERTIFICATE_FACTORY);
+      healthInsuranceKeyStoreGenerator.loadHealthInsuranceKeys(keyFilePath);
+      KeyStore keystore = healthInsuranceKeyStoreGenerator.generateKeyStore(keyStorePassword);
 
-          if (shouldEmbedPrivateKey) {
-              Path privateKeyPath = Paths.get(cmd.getOptionValue("private-key"));
-              Path chain = Paths.get(cmd.getOptionValue("chain"));
-              PrivateKeyHandler generator = new PrivateKeyHandler(CERTIFICATE_FACTORY);
-              generator.embedPrivateKeyInKeyStore(keystore, privateKeyPath, chain, privateKeyAlias, keyStorePassword);
-              Logger.debug("Successfully embedded private key in keystore with alias '" + privateKeyAlias + "'");
-          }
-          writeKeyStore(keystore, keyStoreFilePath, keyStorePassword);
-      } catch (SeconKeyStoreGeneratorException e) {
-          Logger.error(e.getMessage());
-          System.exit(1);
+      if (shouldEmbedPrivateKey) {
+        Path privateKeyPath = Paths.get(cmd.getOptionValue("private-key"));
+        Path chain = Paths.get(cmd.getOptionValue("chain"));
+        PrivateKeyHandler generator = new PrivateKeyHandler(CERTIFICATE_FACTORY);
+        generator.embedPrivateKeyInKeyStore(keystore, privateKeyPath, chain, privateKeyAlias,
+            keyStorePassword);
+        Logger.debug(
+            "Successfully embedded private key in keystore with alias '" + privateKeyAlias + "'");
       }
-  }
-    /**
-     * Write new keyStore to disk
-     *
-     * @param keystore the keystore to write
-     * @param storePath the path of the key store file
-     * @param storePassword the password used to generate the key store
-     */
-    static void writeKeyStore(KeyStore keystore, Path storePath, String storePassword) {
-        try (OutputStream outputStream = Files.newOutputStream(storePath)) {
-            keystore.store(outputStream, storePassword.toCharArray());
-        } catch (KeyStoreException e) {
-            throw new SeconKeyStoreGeneratorException(
-                    "Unable to create empty key store instance of type jks: " + e.getMessage(), e);
-        } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
-            throw new SeconKeyStoreGeneratorException("Unable to write key store: " + e.getMessage(), e);
-        }
-
-        Logger.info(
-                "Generated key store '" + storePath + "'");
+      writeKeyStore(keystore, keyStoreFilePath, keyStorePassword);
+    } catch (SeconKeyStoreGeneratorException e) {
+      Logger.error(e.getMessage());
+      System.exit(1);
     }
+  }
+
+  /**
+   * Write new keyStore to disk.
+   *
+   * @param keystore      the keystore to write
+   * @param storePath     the path of the key store file
+   * @param storePassword the password used to generate the key store
+   */
+  static void writeKeyStore(KeyStore keystore, Path storePath, String storePassword) {
+    try (OutputStream outputStream = Files.newOutputStream(storePath)) {
+      keystore.store(outputStream, storePassword.toCharArray());
+    } catch (KeyStoreException e) {
+      throw new SeconKeyStoreGeneratorException(
+          "Unable to create empty key store instance of type jks: " + e.getMessage(), e);
+    } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
+      throw new SeconKeyStoreGeneratorException("Unable to write key store: " + e.getMessage(), e);
+    }
+
+    Logger.info(
+        "Generated key store '" + storePath + "'");
+  }
 }
